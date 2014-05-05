188dc707295eab0ab85977f0ee957c3001d661f5
Endy Muhardin <endy.muhardin@gmail.com>
2014-05-05 18:47:09 +0700

    domain model Pegawai

---
+++ src/main/java/id/go/kemdikbud/tandajasa/domain/Pegawai.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package id.go.kemdikbud.tandajasa.domain;
+
+/**
+ *
+ * @author endy
+ */
+public class Pegawai {
+    private Integer id;
+    private String nip;
+    private String nama;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    
+}
